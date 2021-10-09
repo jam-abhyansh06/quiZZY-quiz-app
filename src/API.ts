6646bda19d6aef357c3383bfea1247e0e94a1c5b
jam-abhyansh06 <abhyansh.5959@gmail.com>
2021-10-09 15:08:01 +0530

    hit the API endpoint

--- src/API.ts
+++ src/API.ts
@@ -1,1 +1,12 @@
-import React from "react";
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&category=31&type=multiple`
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+// components
 import QuestionCard from './components/QuestionCard';
+// types
+import { Difficulty } from './API';
 
 const TOTAL_QUESTIONS = 10;
 
@@ -11,6 +15,9 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+  
 
   const startQuiz = async () => {
 
@@ -32,14 +39,14 @@ const App = () => {
             </button>
             <p className='score'>Score: </p>
             <p>Loading Questions...</p>
-            <QuestionCard
+            {/* <QuestionCard
               questionNumber = {number + 1}
               totalQuestions = {TOTAL_QUESTIONS}
               question = {questions[number].question}
               answers = {questions[number].answers}
               userAnswer = {userAnswers ? userAnswers[number] : undefined}
               callback = {checkAnswer}
-            />
+            /> */}
             <button className='next' onClick={nextQuestion}>
               Next Question
             </button>
